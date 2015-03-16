bca5287c239f1085f4ce5f191212016b2e558154
Julian Duque <julianduquej@gmail.com>
2015-03-16 14:44:07 -0500

    Agregar un index.html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const http = require('http')
+const path = require('path')
+const fs   = require('fs')
 
 const port   = process.env.PORT || 8080
 const server = http.createServer()
@@ -13,9 +15,17 @@ server.on('listening', function () {
 function handleRequest(req, res) {
   let url = req.url
 
+  if (url === '/')
+    return handleIndex(req, res)
+
   res.setHeader('content-type', 'text/plain')
   res.statusCode = 404
   res.end(`${url} not found`)
+}
+
+function handleIndex(req, res) {
+  res.setHeader('content-type', 'text/html')
+  fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res)
 }
 
 server.listen(port)
